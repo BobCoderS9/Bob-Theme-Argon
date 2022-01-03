72e6a68054efc025f61bcbab6aa582037a874580
Bob <bobcoderss@gmail.com>
2022-01-03 21:24:49 +0800

    Update 适配v2baord v1.5.4

--- dashboard.blade.php
+++ dashboard.blade.php
@@ -4,19 +4,20 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="icon" href="/theme/bob-argon/favicon.ico">
+    <link rel="icon" href="/theme/Bob-Theme-Argon/favicon.ico">
     <title>Bob-Argon</title>
-    <link rel="stylesheet" href="https://unpkg.com/element-plus/lib/theme-chalk/index.css">
-    <link href="/theme/Bob-Theme-Argon/css/app.e0a179ca.css" rel="preload" as="style">
+    <link rel="stylesheet" href="//unpkg.com/element-plus/dist/index.css" />
+    <link href="/theme/Bob-Theme-Argon/css/app.8acf9b4b.css" rel="preload" as="style">
     <link href="/theme/Bob-Theme-Argon/css/chunk-vendors.9f69bc1a.css" rel="preload" as="style">
-    <link href="/theme/Bob-Theme-Argon/js/app.0cc1bfa6.js" rel="preload" as="script">
-    <link href="/theme/Bob-Theme-Argon/js/chunk-vendors.59acb532.js" rel="preload" as="script">
+    <link href="/theme/Bob-Theme-Argon/js/app.ae83a528.js" rel="preload" as="script">
+    <link href="/theme/Bob-Theme-Argon/js/chunk-vendors.82406648.js" rel="preload" as="script">
     <link href="/theme/Bob-Theme-Argon/css/chunk-vendors.9f69bc1a.css" rel="stylesheet">
-    <link href="/theme/Bob-Theme-Argon/css/app.e0a179ca.css" rel="stylesheet">
+    <link href="/theme/Bob-Theme-Argon/css/app.8acf9b4b.css" rel="stylesheet">
 </head>
 <body>
 <div id="app"></div>
 <script>
+    // 首页订阅客户端显示
     window.CLIENT = {
         'SSR': true,
         'Clash': false,
@@ -38,8 +39,9 @@
             s.async = 1;
             d.getElementsByTagName("head")[0].appendChild(s);
         })();
-    }</script>
-<script src="/theme/Bob-Theme-Argon/js/chunk-vendors.59acb532.js"></script>
-<script src="/theme/Bob-Theme-Argon/js/app.0cc1bfa6.js"></script>
+    }
+</script>
+<script src="/theme/Bob-Theme-Argon/js/chunk-vendors.82406648.js"></script>
+<script src="/theme/Bob-Theme-Argon/js/app.ae83a528.js"></script>
 </body>
 </html>
